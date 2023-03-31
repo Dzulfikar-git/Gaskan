479d6b80f9948bdd98b0e01664d061ae0149d18b
Dzulfikar <kardzulfi@gmail.com>
2023-03-31 21:04:31 +0700

    initial commit

---
+++ Gaskan/GaskanApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaskanApp.swift
+//  Gaskan
+//
+//  Created by Dzulfikar on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GaskanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
