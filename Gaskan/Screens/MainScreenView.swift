f69436956d7279e515eb45a86987f18fc17851ef
Dzulfikar <kardzulfi@gmail.com>
2023-03-31 21:36:59 +0700

    Add initial screens and assets

---
+++ Gaskan/Screens/MainScreenView.swift
@@ -0,0 +1,21 @@
+//
+//  MainScreenView.swift
+//  Gaskan
+//
+//  Created by Dzulfikar on 31/03/23.
+//
+//  This MainScreenView will be used as `EntryPoint` of our app.
+
+import SwiftUI
+
+struct MainScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreenView()
+    }
+}
