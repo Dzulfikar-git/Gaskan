88f6a74bc43621a96ec8a9f6937314f47c1b688a
Muhammad Adha Fajri Jonison <madhafajrij@gmail.com>
2023-04-19 10:35:20 +0700

    change comma to period on text field input

--- Gaskan/Utils/TextFieldUtil.swift
+++ Gaskan/Utils/TextFieldUtil.swift
@@ -13,13 +13,14 @@ struct TextFieldUtil {
     // This function is used to handle duplicate decimal. It will match the decimal pattern of `.`
     // If match count >= 1, then it will drop the new decimal input.
     static func handleDecimalInput(value: String) -> String {
+        let modifiedValue = value.replacingOccurrences(of: ",", with: ".") // replace commas with decimal points
         let pattern = "\\.(?=.*\\.)"
         let regex = try! NSRegularExpression(pattern: pattern, options: .caseInsensitive)
-        let matches = regex.matches(in: value, options: [], range: NSRange(location: 0, length: value.utf16.count))
+        let matches = regex.matches(in: modifiedValue, options: [], range: NSRange(location: 0, length: modifiedValue.utf16.count))
         if matches.count >= 1 {
             // input value has more than 1 decimal place
-            return String(value.dropLast())
+            return String(modifiedValue.dropLast())
         }
-        return String(value)
+        return String(modifiedValue)
     }
 }
